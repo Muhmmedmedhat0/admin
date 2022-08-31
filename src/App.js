51e4a4fb0d8f6f3e16676c7c9443bcf540f74bba
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-08-31 18:01:31 +0200

    protected rouets

--- src/App.js
+++ src/App.js
@@ -12,17 +12,21 @@ import NewProduct from './pages/newProduct/NewProduct';
 import Login from './pages/login/login';
 
 function App() {
+  const ADMIN =
+    JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)?.userInfo
+      .isAdmin || false;
+  console.log(ADMIN);
   return (
     <Router>
       <Topbar />
       <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/users" element={<UserList />} />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="/newUser" element={<NewUser />} />
-        <Route path="/products" element={<ProductList />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/newproduct" element={<NewProduct />} />
+        <Route index exact path="/" element={<Home />} />
+        {ADMIN && <Route path="/users" element={<UserList />} />}
+        {ADMIN && <Route path="/user/:id" element={<User />} />}
+        {ADMIN && <Route path="/newUser" element={<NewUser />} />}
+        {ADMIN && <Route path="/products" element={<ProductList />} />}
+        {ADMIN && <Route path="/product/:id" element={<Product />} />}
+        {ADMIN && <Route path="/newproduct" element={<NewProduct />} />}
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
