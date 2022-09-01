bd3c4489fc9fb3a459d4c193e18da9f176620f61
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-09-01 16:47:38 +0200

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Topbar from './components/topbar/Topbar';
 import './App.css';
 
 import Home from './pages/home/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
@@ -10,23 +10,22 @@ import ProductList from './pages/productList/ProductList';
 import Product from './pages/product/Product';
 import NewProduct from './pages/newProduct/NewProduct';
 import Login from './pages/login/login';
+import { useSelector } from 'react-redux';
 
 function App() {
-  const ADMIN =
-    JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)?.userInfo
-      .isAdmin || false;
+  const { userInfo } = useSelector((state) => state.user);
   return (
     <Router>
       <Topbar />
       <Routes>
-        <Route index exact path="/" element={<Home />} />
-        {ADMIN && <Route path="/users" element={<UserList />} />}
-        {ADMIN && <Route path="/user/:id" element={<User />} />}
-        {ADMIN && <Route path="/newUser" element={<NewUser />} />}
-        {ADMIN && <Route path="/products" element={<ProductList />} />}
-        {ADMIN && <Route path="/product/:id" element={<Product />} />}
-        {ADMIN && <Route path="/newproduct" element={<NewProduct />} />}
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={userInfo?.isAdmin ? <Home /> : <Navigate to="/login" />}/>
+        <Route path="/users" element={userInfo?.isAdmin ? <UserList /> : <Navigate to="/login" />}/>
+        <Route path="/user/:id" element={userInfo?.isAdmin ? <User /> : <Navigate to="/login" />}/>
+        <Route path="/newUser" element={userInfo?.isAdmin ? <NewUser /> : <Navigate to="/login" />} />
+        <Route path="/products" element={userInfo?.isAdmin ? <ProductList /> : <Navigate to="/login" />}/>
+        <Route path="/product/:id" element={userInfo?.isAdmin ? <Product /> : <Navigate to="/login" />}/>
+        <Route path="/newproduct" element={userInfo?.isAdmin ? <NewProduct /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!userInfo?.isAdmin ? <Login /> : <Navigate to="/" />}/>
       </Routes>
     </Router>
   );
